a44fa481045829cb5454de75299d1cfc3d4bbd82
slama-pakhrin <salonlama47@gmail.com>
2024-02-12 23:01:48 -0500

    Projects Section Initialized

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ function dropdown(){
     const menu = document.querySelector(".hamburger-links")
     menu.classList.toggle("open")
 }
+
+// function projectsView(){
+//     const project = document.querySelector(".arrow-down")
+//     project.classList.toggle("open")
+// }
