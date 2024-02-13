a78df4456ad3d3262dd026d98d8aa4aa3ea472e3
slama-pakhrin <salonlama47@gmail.com>
2024-02-13 04:48:06 -0500

    JS functionality

--- server.js
+++ server.js
@@ -7,3 +7,23 @@ function dropdown(){
 //     const project = document.querySelector(".arrow-down")
 //     project.classList.toggle("open")
 // }
+
+var scrollView = document.getElementById('clickforProject')
+scrollView.addEventListener('click', function (event){
+    event.preventDefault()
+
+    var targetView = document.getElementById('project-section')
+    targetView.scrollIntoView({
+        behavior: "smooth" 
+    })
+})
+
+var downView = document.getElementById('arrow-down')
+downView.addEventListener('click', function(event){
+    event.preventDefault()
+
+    var targetedView = document.getElementById('project-section')
+    targetedView.scrollIntoView({
+        behavior: "smooth"
+    })
+})
